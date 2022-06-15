a4070f3d2f1cf1a5fc97d1439595cfad8b3b7dbc
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-16 02:05:26 +0300

    chore: initial commit of mvvm pattern

---
+++ test/feature/login/login_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  setUp(() {
+    print('here');
+  });
+
+  test("User Login Fail Test", () {
+    final isUserLogin = true;
+    expect(isUserLogin, isTrue);
+  });
+
+  group("User Login Ful Test", () {
+    //MARK: test1
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    });
+
+    //MARK: test2
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    });
+
+    //MARK: test3
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    });
+  });
+}
