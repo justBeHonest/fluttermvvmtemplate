515baf99a62a66328c41ee6aaba7414efee32504
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-19 23:49:00 +0300

    chore: add theme notifier in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermvvmtemplate/core/init/notifier/provider_list.dart';
+import 'package:fluttermvvmtemplate/core/init/notifier/theme_notifier.dart';
 import 'package:fluttermvvmtemplate/view/authenticate/test/view/test_view.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TestView();
+    return MultiProvider(
+      providers: [...ApplicationProvider.instance.dependItems],
+      child: MaterialApp(
+        home: TestView(),
+        theme: Provider.of<ThemeNotifier>(context, listen: false).currentTheme,
+      ),
+    );
   }
 }
