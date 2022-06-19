26b88790c8914db70c3c97c53f0351b7abee826b
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-20 01:50:03 +0300

    chore: test view model

--- lib/view/authenticate/test/viewmodel/test_view_model.dart
+++ lib/view/authenticate/test/viewmodel/test_view_model.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fluttermvvmtemplate/core/init/notifier/theme_notifier.dart';
 import 'package:mobx/mobx.dart';
+import 'package:provider/provider.dart';
 part 'test_view_model.g.dart';
 
 class TestViewModel = _TestViewModelBase with _$TestViewModel;
 
 abstract class _TestViewModelBase with Store {
+  // This is for set context actually you should inherit this class from BaseView
+  // late BuildContext context;
+
+  // void setContext(BuildContext context) {
+  //   this.context = context;
+  // }
+
   // core ise
   @observable
   int number = 0;
@@ -17,4 +27,10 @@ abstract class _TestViewModelBase with Store {
   void incrementNumber() {
     number++;
   }
+
+  // This is for change theme
+  // void changeTheme() {
+  //   Provider.of<ThemeNotifier>(context, listen: false)
+  //       .changeValue(AppThemes.DARK);
+  // }
 }
