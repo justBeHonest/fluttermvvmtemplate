4572a6a1fb513fead945c258fa4c6f2e9d2f17b9
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-20 01:48:08 +0300

    chore: not found ui

---
+++ lib/core/components/card/not_found.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NotFoundNavigationWidget extends StatelessWidget {
+  const NotFoundNavigationWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Text("Not Found"));
+}
