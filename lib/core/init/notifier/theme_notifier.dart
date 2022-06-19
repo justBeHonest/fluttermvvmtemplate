708d20f80a79771d30243606c8b7200ba3b37dd4
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-19 23:48:44 +0300

    chore: theme notifier

---
+++ lib/core/init/notifier/theme_notifier.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermvvmtemplate/core/init/theme/app_theme.dart';
+import 'package:fluttermvvmtemplate/core/init/theme/app_theme_light.dart';
+
+import '../../constants/enums/app_theme_enum.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  ThemeData _currnetTheme = AppThemeLight.instance.theme;
+  ThemeData get currentTheme => AppThemeLight.instance.theme;
+
+  void changeValue(AppThemes themes) {
+    if (themes == AppThemes.LIGHT) {
+      _currnetTheme = ThemeData.light();
+    } else {
+      _currnetTheme = ThemeData.dark();
+    }
+    notifyListeners();
+  }
+}
