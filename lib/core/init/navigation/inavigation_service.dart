4ab2551c0240d4e56685731d130de2b22802a61a
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-20 01:49:27 +0300

    chore: navigation manager interface

---
+++ lib/core/init/navigation/inavigation_service.dart
@@ -0,0 +1,10 @@
+abstract class INavigationService {
+  Future<void> navigateToPage({
+    required String path,
+    required Object data,
+  });
+  Future<void> navigateToPageClear({
+    required String path,
+    required Object data,
+  });
+}
