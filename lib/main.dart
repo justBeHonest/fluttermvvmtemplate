95cdd026911f4674ce7774e4c29641c628a2f3b7
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-20 01:48:29 +0300

    chore: cache manager with shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermvvmtemplate/core/init/navigation/navigation_route.dart';
+import 'package:fluttermvvmtemplate/core/init/navigation/navigation_service.dart';
 import 'package:fluttermvvmtemplate/core/init/notifier/provider_list.dart';
 import 'package:fluttermvvmtemplate/core/init/notifier/theme_notifier.dart';
 import 'package:fluttermvvmtemplate/view/authenticate/test/view/test_view.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+import 'core/init/cache/local_manager.dart';
+
+void main() {
+  LocalManager.preferencesInit();
+  runApp(
+    MultiProvider(
+      providers: [...ApplicationProvider.instance.dependItems],
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [...ApplicationProvider.instance.dependItems],
-      child: MaterialApp(
-        home: TestView(),
-        theme: Provider.of<ThemeNotifier>(context, listen: false).currentTheme,
-      ),
+    return MaterialApp(
+      home: const TestView(),
+      theme: Provider.of<ThemeNotifier>(context, listen: false).currentTheme,
+      navigatorKey: NavigationService.instance.navigatorKey,
+      onGenerateRoute: NavigationRoute.instance.generateRoute,
     );
   }
 }
