2fdd8cf5b847fb9420436fdbfb2b032cb4901684
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-19 22:45:47 +0300

    chore: multi language programming

---
+++ lib/core/init/lang/language_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LanguageManager {
+  // Lazy singleton
+  // static LanguageManager? _instance;
+  // static LanguageManager get instance {
+  //   _instance ??= LanguageManager._init();
+  //   return _instance!;
+  // }
+
+  // LanguageManager._init();
+
+}
