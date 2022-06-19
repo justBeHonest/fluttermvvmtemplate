687dd33d15edf85d7765e4f0888636ee461b6928
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-20 01:48:48 +0300

    chore: Navigation manager setup

---
+++ lib/core/init/navigation/navigation_route.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermvvmtemplate/core/components/card/not_found.dart';
+import 'package:fluttermvvmtemplate/core/constants/navigation/navigation_constants.dart';
+import 'package:fluttermvvmtemplate/view/authenticate/test/view/test_view.dart';
+
+class NavigationRoute {
+  static NavigationRoute _instance = NavigationRoute._init();
+  static NavigationRoute get instance => _instance;
+
+  NavigationRoute._init();
+
+  Route<dynamic> generateRoute(RouteSettings args) {
+    switch (args.name) {
+      case NavigationConstants.testView:
+        return normalNavigate(TestView());
+      default:
+        return MaterialPageRoute(
+          builder: (context) => NotFoundNavigationWidget(),
+        );
+    }
+  }
+
+  MaterialPageRoute normalNavigate(Widget widget) =>
+      MaterialPageRoute(builder: (context) => widget);
+}
---
+++ lib/core/init/navigation/navigation_service.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fluttermvvmtemplate/core/init/navigation/inavigation_service.dart';
+
+class NavigationService implements INavigationService {
+  static NavigationService _instance = NavigationService._init();
+  static NavigationService get instance => _instance;
+
+  NavigationService._init();
+
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+  final removeAllOldRoutes = (Route<dynamic> route) => false;
+
+  @override
+  Future<void> navigateToPage({
+    required String path,
+    required Object data,
+  }) async {
+    await navigatorKey.currentState!.pushNamed(path, arguments: data);
+  }
+
+  @override
+  Future<void> navigateToPageClear({
+    required String path,
+    required Object data,
+  }) async {
+    await navigatorKey.currentState!.pushNamedAndRemoveUntil(
+      path,
+      removeAllOldRoutes,
+      arguments: data,
+    );
+  }
+}
