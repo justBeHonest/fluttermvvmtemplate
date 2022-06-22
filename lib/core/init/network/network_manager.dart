a223a829ef15f25f4945423642b1a07cbeabe430
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2022-06-22 12:58:54 +0300

    chore: networkl layer

--- lib/core/init/network/network_manager.dart
+++ lib/core/init/network/network_manager.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:fluttermvvmtemplate/core/base/model/base_error.dart';
+import 'package:fluttermvvmtemplate/core/base/model/base_model.dart';
 import 'package:fluttermvvmtemplate/core/constants/enums/preferences_keys.dart';
 import 'package:fluttermvvmtemplate/core/init/cache/locale_manager.dart';
 
@@ -16,8 +20,36 @@ class NetworkManager {
         "val": LocaleManager.instance.getStringValue(PreferencesKeys.TOKEN)
       },
     );
-    dio = Dio(baseOptions);
+    _dio = Dio(baseOptions);
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        options..path += "veli";
+        //options.
+      },
+      // onResponse: (e, handler) {
+      //   // gelen json datasını modellere çevirme işlemi burada olabilir
+      // },
+      onError: (e, handler) {
+        // return BaseError(e.message);
+      },
+    ));
   }
 
-  late Dio dio;
+  late Dio _dio;
+
+  Future dioGet<T extends BaseModel>(String path, T model) async {
+    final response = await _dio.get(path);
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        final responseBody = response.data;
+        if (responseBody is List) {
+          return responseBody.map((e) => model.fromJson(e)).toList();
+        } else if (responseBody is Map) {
+          return model.fromJson(responseBody as Map<String, Object>);
+        }
+        return responseBody;
+        break;
+      default:
+    }
+  }
 }
